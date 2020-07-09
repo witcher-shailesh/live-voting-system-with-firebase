4246c07598b7d70668ed2fab947a66dc9bff1f75
shailesh gahlawat <shailesh.techcourse@gmail.com>
2020-07-09 23:03:06 +0530

    changes

---
+++ firebaseDemo01/android/app/src/main/kotlin/com/example/firebaseDemo01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebaseDemo01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- firebasePractice/android/app/src/main/kotlin/com/example/firebasePractice/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.firebasePractice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
